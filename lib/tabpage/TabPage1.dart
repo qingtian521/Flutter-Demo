53e54fa57ace491a0cbf543822710a3aa1fdf6d7
renlei <572159012@qq.com>
2019-01-18 17:59:18 +0800

    第一次提交

---
+++ lib/tabpage/TabPage1.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class TabPage1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  Center(
+      child: Text("这是tab1"),
+    );
+  }
+
+}
---
+++ lib/tabpage/TabPage2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TabPage2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  Center(
+      child: Text("这是tab2"),
+    );
+  }
+
+}
